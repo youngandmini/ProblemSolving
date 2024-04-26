53ae10f89f6771d57fdf68bb485bbfd460f704bb
Lee Youngmin <80088671+youngandmini@users.noreply.github.com>
2024-04-26 10:11:29 +0900

    [Silver IV] Title: 상근이의 여행, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,9 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for repeat in range(t):
+    n,m = map(int, sys.stdin.readline().split())
+    for i in range(m):
+        a,b = map(int, sys.stdin.readline().split())
+    print(n-1)
